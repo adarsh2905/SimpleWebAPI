0f25a29d0099bd2e9810285b123968c90b1e52bc
adarsh.k <adarsh.k@TL308>
2022-09-02 10:35:41 +0530

    First commit

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using DAL.Models;
+using DAL.ProductRepositories;
+using System.Diagnostics;
+using System.Xml.Linq;
+
+namespace API.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        //Product[] products = new Product[]
+        //{
+        //    new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
+        //    new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M },
+        //    new Product { Id = 3, Name = "Hammer", Category = "Hardware", Price = 16.99M }
+        //};
+
+        string _conStr = ConfigurationManager.ConnectionStrings["Demo"].ConnectionString;
+
+        // GET api/<controller>
+        public IHttpActionResult GetAllProducts()
+        {
+
+            ProductRepository repo = new ProductRepository(_conStr);
+
+            if(repo != null)
+            {
+                return Ok(repo.GetAllProducts());
+            }
+            return NotFound();
+        }
+
+        // GET api/<controller>/5
+        public IHttpActionResult GetProduct(int id)
+        {
+
+            ProductRepository repo = new ProductRepository(_conStr);
+            var product = repo.GetProductById(id);
+            if (product.Id == 0)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        // POST api/<controller>
+        public IHttpActionResult Post([FromBody] ProductDbo product)
+        {
+            ProductRepository repo = new ProductRepository(_conStr);
+            int insertedProduct = repo.PostProduct(product);
+            product.Id = insertedProduct;
+            if(insertedProduct == 0)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+
+        // PUT api/<controller>/5
+        public IHttpActionResult Put(int id, [FromBody] ProductDbo product)
+        {
+            ProductRepository repo = new ProductRepository(_conStr);
+            int result = repo.UpdateProduct(id, product);
+
+            if(result == 0)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        // DELETE api/<controller>/5
+        public IHttpActionResult Delete(int id)
+        {
+            ProductRepository repo = new ProductRepository(_conStr);
+            int result = repo.DeleteProduct(id);
+
+            if (result == 0)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+    }
+}
---
+++ DAL/ProductRepositories/ProductRepository.cs
@@ -0,0 +1,115 @@
+﻿using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.ProductRepositories
+
+{
+    public class ProductRepository
+    {
+        private string _conStr = null;
+
+        public ProductRepository(string conStr)
+        {
+            this._conStr = conStr;
+        }
+
+        
+        public List<ProductDbo> GetAllProducts()
+        {
+
+            List<ProductDbo> products = new List<ProductDbo>();
+            string displayQuery = "SELECT * FROM Product";
+            using (SqlConnection sqlConnection = new SqlConnection(_conStr))
+            {
+                sqlConnection.Open();
+                SqlCommand displayCommand = new SqlCommand(displayQuery, sqlConnection);
+                SqlDataReader dataReader = displayCommand.ExecuteReader();
+                while (dataReader.Read())
+                {
+                    products.Add(new ProductDbo
+                    {
+                        Id = Convert.ToInt32(dataReader["Id"]),
+                        Name = dataReader["Name"].ToString(),
+                        Category = dataReader["Category"].ToString(),
+                        Price = Convert.ToDecimal(dataReader["Price"])
+                    });
+                }
+                dataReader.Close();
+                return products;
+            }
+        }
+
+        public ProductDbo GetProductById(int id)
+        {
+            ProductDbo product = new ProductDbo();
+            string displaySingleUser = "SELECT * FROM Product WHERE Id = @id";
+            using (SqlConnection sqlConnection = new SqlConnection(_conStr))
+            {
+                sqlConnection.Open();
+                SqlCommand displayCommand = new SqlCommand(displaySingleUser, sqlConnection);
+                displayCommand.Parameters.AddWithValue("@id", id);
+                SqlDataReader dataReader = displayCommand.ExecuteReader();
+                if (dataReader.Read())
+                {
+                    product.Id = Convert.ToInt32(dataReader["Id"]);
+                    product.Name = dataReader["Name"].ToString();
+                    product.Category = dataReader["Category"].ToString();
+                    product.Price = Convert.ToDecimal(dataReader["Price"]);
+                }
+                dataReader.Close();
+                return product;
+            }     
+        }
+
+        public int PostProduct(ProductDbo product)
+        {
+            ProductDbo productDbo = new ProductDbo();
+            string insertQuery = "INSERT INTO Product(Name, Category, Price) VALUES(@Name, @Category, @Price); select SCOPE_IDENTITY();";
+            using (SqlConnection sqlConnection = new SqlConnection(_conStr))
+            {
+                sqlConnection.Open();
+                SqlCommand insertCommand = new SqlCommand(insertQuery, sqlConnection);
+                insertCommand.Parameters.AddWithValue("@Name", product.Name);
+                insertCommand.Parameters.AddWithValue("@Category", product.Category);
+                insertCommand.Parameters.AddWithValue("@Price", product.Price);
+
+                int idOfInsertedProduct =  Convert.ToInt32(insertCommand.ExecuteScalar());
+                return idOfInsertedProduct;
+            }
+        }
+
+        public int UpdateProduct(int id, ProductDbo product)
+        {
+            string updateQuery = "UPDATE Product SET Name=@Name, Category = @Category, Price = @Price WHERE Id = @id";
+            using (SqlConnection sqlConnection = new SqlConnection(_conStr))
+            {
+                sqlConnection.Open();
+                SqlCommand updateCommand = new SqlCommand(updateQuery, sqlConnection);
+                updateCommand.Parameters.AddWithValue("@Name", product.Name);
+                updateCommand.Parameters.AddWithValue("@Category", product.Category);
+                updateCommand.Parameters.AddWithValue("@Price", product.Price);
+                updateCommand.Parameters.AddWithValue("@Id", id);
+                int noOfAffectedRows = updateCommand.ExecuteNonQuery();
+                return noOfAffectedRows;
+            }
+        }
+
+        public int DeleteProduct(int id)
+        {
+            string DeleteQuery = "DELETE FROM Product WHERE Id = @id";
+            using (SqlConnection sqlConnection = new SqlConnection(_conStr))
+            {
+                sqlConnection.Open();
+                SqlCommand deleteCommand = new SqlCommand(DeleteQuery, sqlConnection);
+                deleteCommand.Parameters.AddWithValue("@id", id);
+                int numberOfAffectedRows = deleteCommand.ExecuteNonQuery();
+                return numberOfAffectedRows;
+            }
+        }
+    }
+}
